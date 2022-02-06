fcabc50f1c716c64cadc1c4e279d2bd8cb28136a
Shinichi <miyakawashi@gmail.com>
2022-02-06 18:37:53 -0500

    post and user api endpoints, model associations

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 require('dotenv').config(); 
 const sequelize = require('./config/connection');
 const PORT = process.env.PORT || 3001;
+const routes = require('./controllers')
 
-sequelize.sync({ force: false })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: true })
     .then(() => {
         app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
     });
