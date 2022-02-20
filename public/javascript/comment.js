a934be22dc781ec6009156f0d3101b8357121b9f
Shinichi <miyakawashi@gmail.com>
2022-02-20 18:59:19 -0500

    post visiblity toggling

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,8 +2,16 @@ async function postClickHandler(event) {
     event.preventDefault();
 
     console.log('Yo you clicked me');
+    const state = document.querySelector('#comment').getAttribute('state');
+    if (state === 'off') {
+        document.querySelector('#comment').setAttribute('class', 'visible');
+        document.querySelector('#comment').setAttribute('state', 'on');
+    } else {
+        document.querySelector('#comment').setAttribute('class', 'hidden'); 
+        document.querySelector('#comment').setAttribute('state', 'off');
+    }
 }
 
-document.querySelectorAll('#post-title').forEach(item => {
+document.querySelectorAll('#post').forEach(item => {
     item.addEventListener('click', postClickHandler) 
 });
